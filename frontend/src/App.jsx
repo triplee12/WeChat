8e5b9187af63f63cb41b03247945788c008e9590
triplee12 <tripleeoliver@gmail.com>
2024-07-16 14:59:46 +0100

    Implemented Signup and Login page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/SignUp'
+
+function App() {
+
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      {/* <Login /> */}
+      <Signup />
+    </div>
+  )
+}
+
+export default App
