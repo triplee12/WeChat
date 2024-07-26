73427588f9ad4a2a272ed7c48a23ede00df53519
triplee12 <tripleeoliver@gmail.com>
2024-07-26 23:28:53 +0100

    Implemented online users functionality

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,4 +1,4 @@
-import { Server } from 'socket.io';
+const { Server } = require('socket.io');
 const http = require('http');
 const express = require('express');
 
@@ -10,10 +10,18 @@ const io = new Server(server, {
         methods: ["GET", "POST"],
     }
 });
+const userSocketMap = {}
 io.on('connection', (socket) => {
     console.log("User connected", socket.id);
+    const userId = socket.handshake.query.userId;
+    if (userId != "undefined") {
+        userSocketMap[userId] = socket.id;
+    }
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
     socket.on("disconnect", () => {
-        console.log("User disconnected", socket.id)
+        console.log("User disconnected", socket.id);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
     });
 });
 
